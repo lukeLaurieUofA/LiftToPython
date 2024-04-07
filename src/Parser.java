1b8fca203764241ed6f5e9e61e333c34059618b6
RyanBullard <cooldudebullard@gmail.com>
2024-04-07 16:06:34 -0700

    Split up the values methods, added support for strings and math expressions.

--- src/Parser.java
+++ src/Parser.java
@@ -11,8 +11,10 @@ public class Parser {
     private static final Pattern var = Pattern.compile("^pecs(\\d)*$|^delts(\\d)*$|^lats(\\d)*$|^biceps(\\d)*$|^triceps(\\d)*$|^abs(\\d)*$|^obliques(\\d)*$|^quads(\\d)*$|^hamstrings(\\d)*$|^" +
             "glutes(\\d)*$|^calves(\\d)*$|^forearms(\\d)*$");
     private static final Pattern intVal = Pattern.compile("^\\d+$|^-\\d+$");
-    private static final Pattern bool = Pattern.compile("^t$|^f$");
+    private static final Pattern strVal = Pattern.compile("^\"(.*)\"$");
+    private static final Pattern bool = Pattern.compile("^gotItUp$|^failed$");
     private static final Pattern end_scope = Pattern.compile("^rightWeightClip$");
+    private static final Pattern int_expr = Pattern.compile("^(.+) creatine (.+)|(.+) restDay (.+)|(.+) steroids (.+)|(.+) vegan (.+)|(.+) muscleMass (.+)$");
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -44,10 +46,7 @@ public class Parser {
 
     private static boolean endScope(String cmd) {
         Matcher m = end_scope.matcher(cmd);
-        boolean match = false;
-        if(m.find()) {
-            match = true;
-        }
+        boolean match = m.find();
         printMsg(match, "\n<end_scope>", cmd, "end of scope");
         return match;
     }
@@ -56,8 +55,7 @@ public class Parser {
         Matcher m = loop_dec.matcher(cmd);
         boolean match = false;
         if(m.find()) {
-            match = true;
-            match = match && var(m.group(1)); // group 1 is variable name, group 2 is first bound, group 3 is second bound
+            match = var(m.group(1)); // group 1 is variable name, group 2 is first bound, group 3 is second bound
             match = match && val(m.group(2));
             match = match && val(m.group(3));
         }
@@ -69,8 +67,7 @@ public class Parser {
         Matcher m = func_dec.matcher(cmd);
         boolean match = false;
         if (m.find()) { // group 1 is the type, group 2 is the name, group 3 is the parameters
-            match = true;
-            match = match && type(m.group(1)); // No need to check group two, it's always valid if found
+            match = type(m.group(1)); // No need to check group two, it's always valid if found
             match = match && varDecList(m.group(3));
         }
         printMsg(match, "\n<func_dec>", cmd, "function declaration");
@@ -92,8 +89,8 @@ public class Parser {
     private static boolean varDecList(String cmd) {
         String[] split = cmd.split(", ");
         boolean match = true;
-        for (int i = 0; i < split.length; i++) {
-            match = match && varDec(split[i]);
+        for (String s : split) {
+            match = match && varDec(s);
         }
         printMsg(match, "<var_dec_list>", cmd, "variable declaration list");
         return match;
@@ -103,8 +100,7 @@ public class Parser {
         boolean match = false;
         Matcher m = type_var_dec.matcher(cmd);
         if (m.find()) {
-            match = true;
-            match = match && type(m.group(1));
+            match = type(m.group(1));
             match = match && var(m.group(2));
         } else
             match = var(cmd);
@@ -129,29 +125,50 @@ public class Parser {
     private static boolean valList(String cmd) {
         String[] split = cmd.split(", ");
         boolean match = true;
-        for (int i = 0; i < split.length; i++) {
-            match = match && val(split[i]);
+        for (String s : split) {
+            match = match && val(s);
         }
         printMsg(match, "<val_list>", cmd, "value list");
         return match;
     }
 
     private static boolean val(String cmd) {
-        Matcher m = intVal.matcher(cmd);
-        boolean match = m.find();
-        if (match)
-            printMsg(match, "<int>", cmd, "integer");
-        else {
-            m = bool.matcher(cmd);
-            match = m.find();
-            if (match)
-                printMsg(match, "<bool>", cmd, "boolean");
-            else {
-                m = var.matcher(cmd);
-                match = m.find();
-                if (match)
-                    printMsg(match, "<var>", cmd, "variable");
+        boolean match = false;
+        if (intVal(cmd)) {
+            match = true;
+        } else if (bool_val(cmd)) {
+            match = true;
+        } else if(var(cmd)) {
+            match = true;
+        } else if(str_val(cmd)) {
+            match = true;
+        } else if(intExpression(cmd)) {
+            match = true;
+        }
+        printMsg(match, "<val>", cmd, "value");
+        return match;
+    }
+
+    private static boolean str_val(String cmd) {
+        Matcher m = strVal.matcher(cmd);
+        boolean match = false;
+        if(m.find()) {
+            match = true;
+            Matcher m2 = Pattern.compile("(?<!\\\\)\"").matcher(m.group(1));
+            if(m2.find()) {
+                match = false;
             }
+        }
+        printMsg(match, "\n<str_val>", cmd, "string value");
+        return match;
+    }
+
+    private static boolean bool_val(String cmd) {
+        Matcher m = bool.matcher(cmd);
+        boolean match = false;
+        if(m.find()) {
+            match = true;
+            printMsg(true, "<bool>", cmd, "boolean");
         }
         printMsg(match, "<val>", cmd, "value");
         return match;
@@ -162,6 +179,28 @@ public class Parser {
             System.out.println(ntName + ": " + cmd);
         else
             System.out.println("Failed to parse: {" + cmd + "} is not a valid " + item + ".");
+    }
+
+    private static boolean intVal(String cmd) {
+        Matcher m = intVal.matcher(cmd);
+        boolean match = m.find();
+        if (match) {
+            printMsg(true, "<int>", cmd, "integer");
+        }
+        printMsg(match, "<val>", cmd, "integer value");
+        return match;
+    }
+
+    private static boolean intExpression(String cmd) {
+        boolean match = false;
+        Matcher m = int_expr.matcher(cmd);
+        if (m.find()) {
+            //can either match integer values or more integer expressions
+            match = intVal(m.group(1)) || intVal(m.group(1));
+            match = (match && intVal(m.group(2))) || (match && intVal(m.group(2)));
+        }
+        printMsg(match, "<int_expr>", cmd, "integer expression");
+        return match;
     }
 }
     
