9de5166eeb211a0e3d41664ece0545e5175a57ee
RyanBullard <cooldudebullard@gmail.com>
2024-04-07 15:11:28 -0700

    Added patterns for functions, loops, and end of scope.

--- src/Parser.java
+++ src/Parser.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 
 public class Parser {
     private static final Pattern var_assign = Pattern.compile("^(.+) loadBar (.+) pump$");
+    private static final Pattern func_dec = Pattern.compile("^workout (.+) (.+)[(](.+)[)] leftWeightClip$");
+    private static final Pattern loop_dec = Pattern.compile("^set (.+), (.+) to (.+) leftWeightClip$");
     private static final Pattern type_var_dec = Pattern.compile("^(\\w+) (\\w+)$");
     private static final Pattern type = Pattern.compile("^ryanBullard$|^lightWeight$|^weight$|^cables$|^pr$|^samSulek$");
     private static final Pattern var = Pattern.compile("^pecs(\\d)*$|^delts(\\d)*$|^lats(\\d)*$|^biceps(\\d)*$|^triceps(\\d)*$|^abs(\\d)*$|^obliques(\\d)*$|^quads(\\d)*$|^hamstrings(\\d)*$|^" +
             "glutes(\\d)*$|^calves(\\d)*$|^forearms(\\d)*$");
     private static final Pattern intVal = Pattern.compile("^\\d+$|^-\\d+$");
     private static final Pattern bool = Pattern.compile("^t$|^f$");
+    private static final Pattern end_scope = Pattern.compile("^rightWeightClip$");
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -23,8 +26,55 @@ public class Parser {
     }
 
     private static void parseCmd(String cmd) {
-        varAssign(cmd);
+        if(varAssign(cmd)) {
+            System.out.println("<stmt>");
+            return;
+        } else if(funcDec(cmd)) {
+            System.out.println("<stmt>");
+            return;
+        } else if(loopDec(cmd)) {
+            System.out.println("<stmt>");
+            return;
+        } else if(endScope(cmd)) {
+            System.out.println("<stmt>");
+            return;
+        }
         System.out.println("<stmt>");
+    }
+
+    private static boolean endScope(String cmd) {
+        Matcher m = end_scope.matcher(cmd);
+        boolean match = false;
+        if(m.find()) {
+            match = true;
+        }
+        printMsg(match, "\n<end_scope>", cmd, "end of scope");
+        return match;
+    }
+
+    private static boolean loopDec(String cmd) {
+        Matcher m = loop_dec.matcher(cmd);
+        boolean match = false;
+        if(m.find()) {
+            match = true;
+            match = match && var(m.group(1)); // group 1 is variable name, group 2 is first bound, group 3 is second bound
+            match = match && val(m.group(2));
+            match = match && val(m.group(3));
+        }
+        printMsg(match, "\n<loop_dec>", cmd, "loop declaration");
+        return match;
+    }
+
+    private static boolean funcDec(String cmd) {
+        Matcher m = func_dec.matcher(cmd);
+        boolean match = false;
+        if (m.find()) { // group 1 is the type, group 2 is the name, group 3 is the parameters
+            match = true;
+            match = match && type(m.group(1)); // No need to check group two, it's always valid if found
+            match = match && varDecList(m.group(3));
+        }
+        printMsg(match, "\n<func_dec>", cmd, "function declaration");
+        return match;
     }
 
     private static boolean varAssign(String cmd) {
